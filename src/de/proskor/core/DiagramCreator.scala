3441d60e22f283302cf1c2e79ca425fb4c9e5a88
Andrey Skorikov <proskorik@gmail.com>
2012-02-09 01:36:15 +0100

    recursive layout algorithm

--- src/de/proskor/core/DiagramCreator.scala
+++ src/de/proskor/core/DiagramCreator.scala
@@ -19,15 +19,21 @@ object DiagramCreator {
   var verticalMargin = 80
   var horizontalSpacing = 40
   var verticalSpacing = 80
-  var portSpacing = 100
+  var portSpacing = 50
   var subHeight = 80
   var gateHeight = 40
   var gateWidth = 40
   var eventHeight = 40
   var eventWidth = 40
+  var recursive = true
 
   def calculateElementSize(element: Element): (Int, Int) = element match {
-    case component: Component => ((component.inputs.size max component.outputs.size) * portSpacing, subHeight)
+    case component: Component => {
+      if (recursive && !(component.events.isEmpty && component.gates.isEmpty && component.components.isEmpty))
+        calculateComponentSize(component)
+      else
+        (((component.inputs.size max component.outputs.size) + 1) * portSpacing, subHeight)
+    }
     case gate: Gate => (gateWidth, gateHeight)
     case event: Event => (eventWidth, eventHeight)
   }
@@ -40,52 +46,64 @@ object DiagramCreator {
     layers.map(calculateLayerSize).foldLeft((0, verticalMargin * 2 - verticalSpacing))((top, bottom) => (top._1 max bottom._1, top._2 + verticalSpacing + bottom._2))
   }
 
-  def create(component: Component, diagram: Diagram) {
+  def calculateComponentSize(component: Component): (Int, Int) = {
+    val kids: Seq[Element] = (component.gates ::: component.components ::: component.events)
+    val layers = kids.groupBy(depth(_, kids.toSet))
+    calculateComponentSize(layers.values.toSeq)
+  }
+
+  def depth(element: Element, set: Set[Element]): Int = element match {
+    case source: Source => {
+      if (source.targets.toSet.map(targetElement).intersect(set).isEmpty) 0
+      else source.targets.toSet.map(targetElement).intersect(set).map(depth(_, set)).max + 1
+    }
+    case component: Component => {
+      if (component.outputs.isEmpty) 0
+      else component.outputs.map(output => depth(output, set)).max
+    }
+  }
+
+  def targetElement(element: Element) = element match {
+    case gate: Gate => gate
+    case port: Port => port.parent.get.asInstanceOf[Element]
+  }
+
+  def create(component: Component, diagram: Diagram, loff: Int = mainLeft, toff: Int = mainTop, sequence: Int = 100) {
     val repository = new EARepository(component.asInstanceOf[EAComponent].repository)
     val kids: Seq[Element] = (component.gates ::: component.components ::: component.events)
-
-    def depth(element: Element, set: Set[Element]): Int = element match {
-      case source: Source => {
-        if (source.targets.toSet.map(targetElement).intersect(set).isEmpty) 0
-        else source.targets.toSet.map(targetElement).intersect(set).map(depth(_, set)).max + 1
-      }
-      case component: Component => {
-        if (component.outputs.isEmpty) 0
-        else component.outputs.map(output => depth(output, set)).max
-      }
-    }
-
-    def targetElement(element: Element) = element match {
-      case gate: Gate => gate
-      case port: Port => port.parent.get.asInstanceOf[Element]
-    }
-
     val layers = kids.groupBy(depth(_, kids.toSet))
+    val (width, height) = calculateElementSize(component)
     val numLayers = layers.keys.size
-    val (width, height) = calculateComponentSize(layers.values.toSeq)
 
     val componentNode = diagram.addNode
     componentNode.element = component
-    componentNode.sequence = 2
-    componentNode.left = mainLeft
-    componentNode.top = mainTop
+    componentNode.sequence = sequence
+    componentNode.left = loff
+    componentNode.top = toff
     componentNode.width = width
     componentNode.height = height
 
-    addPortsToComponent(componentNode, diagram)
+    addPortsToComponent(componentNode, diagram, sequence - 1)
 
-    var topOffset = mainTop + verticalMargin
+    var topOffset = toff + verticalMargin
     for (i <- 0 until numLayers; layer = layers(i)) {
       val (layerWidth, layerHeight) = calculateLayerSize(layer)
-      var leftOffset = mainLeft + horizontalMargin + (width - layerWidth) / 2
+      var leftOffset = loff + horizontalMargin + (width - layerWidth) / 2
       for (element <- layer) {
         val (elementWidth, elementHeight) = calculateElementSize(element)
         val x = leftOffset
         val y = topOffset + (layerHeight - elementHeight) / 2
-        val node = createNode(element, diagram, x, y, elementWidth, elementHeight)
-        node.element match {
-          case component: Component => addPortsToComponent(node, diagram)
-          case _ =>
+        if (recursive) {
+          element match {
+            case component: Component => create(component, diagram, x, y, sequence - 3)
+            case _ => createNode(element, diagram, x, y, elementWidth, elementHeight, sequence - 2)
+          }
+        } else {
+          val node = createNode(element, diagram, x, y, elementWidth, elementHeight, sequence - 2)
+          element match {
+            case component: Component => addPortsToComponent(node, diagram, sequence - 1)
+            case _ =>
+          }
         }
         leftOffset += (elementWidth + horizontalSpacing)
       }
@@ -93,7 +111,7 @@ object DiagramCreator {
     }
   }
 
-  def addPortsToComponent(node: Node, diagram: Diagram) {
+  def addPortsToComponent(node: Node, diagram: Diagram, sequence: Int) {
     val component = node.element.asInstanceOf[Component]
     val width = node.width
     val top = node.top
@@ -104,7 +122,7 @@ object DiagramCreator {
     for (i <- 0 until component.inputs.size) {
       val input = component.inputs(i)
       val node = diagram.addNode
-      node.sequence = 1
+      node.sequence = sequence
       node.element = input
       node.left = left + (i + 1) * istep - 7
       node.top = bottom - 7
@@ -114,7 +132,7 @@ object DiagramCreator {
     for (i <- 0 until component.outputs.size) {
       val output = component.outputs(i)
       val node = diagram.addNode
-      node.sequence = 1
+      node.sequence = sequence
       node.element = output
       node.left = left + (i + 1) * ostep - 7
       node.top = top - 7
@@ -123,9 +141,9 @@ object DiagramCreator {
     }
   }
 
-  def createNode(element: Element, diagram: Diagram, x: Int, y: Int, width: Int, height: Int): Node = {
+  def createNode(element: Element, diagram: Diagram, x: Int, y: Int, width: Int, height: Int, sequence: Int): Node = {
     val node = diagram.addNode
-    node.sequence = 1
+    node.sequence = sequence
     node.element = element
     node.left = x
     node.top = y
