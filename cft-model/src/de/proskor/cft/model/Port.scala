a1f693c05aa8f1a8f78fdb0f66f6f61540f015e5
Andrey Skorikov <proskorik@gmail.com>
2012-02-23 11:28:44 +0100

    port extractor added

--- cft-model/src/de/proskor/cft/model/Port.scala
+++ cft-model/src/de/proskor/cft/model/Port.scala
@@ -5,3 +5,7 @@ trait Port extends Source {
   def +=(input: Source): Unit
   def -=(input: Source): Unit
 }
+
+object Port {
+  def unapply(port: Port): Option[(String, Option[Source])] = Some(port.name, port.input)
+}
