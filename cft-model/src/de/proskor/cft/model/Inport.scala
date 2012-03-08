8942680ce826f102c619ce3cf4790d453fcad02f
Andrey Skorikov <proskorik@gmail.com>
2012-03-08 01:23:34 +0100

    extractors for inports and outports added

--- cft-model/src/de/proskor/cft/model/Inport.scala
+++ cft-model/src/de/proskor/cft/model/Inport.scala
@@ -5,4 +5,5 @@ trait Inport extends Port
 object Inport {
   def apply(name: String): Inport = Factory.createInport(name)
   def apply(parent: Component, name: String): Inport = Factory.createInport(parent, name)
+  def unapply(inport: Inport): Option[(String, Option[Source])] = Some(inport.name, inport.input)
 }
--- cft-model/src/de/proskor/cft/model/Outport.scala
+++ cft-model/src/de/proskor/cft/model/Outport.scala
@@ -5,4 +5,5 @@ trait Outport extends Port
 object Outport {
   def apply(name: String): Outport = Factory.createOutport(name)
   def apply(parent: Component, name: String): Outport = Factory.createOutport(parent, name)
+  def unapply(outport: Outport): Option[(String, Option[Source])] = Some(outport.name, outport.input)
 }
