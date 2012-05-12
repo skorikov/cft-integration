da2d55a12a358ac92e89ae207a8586fc41e628c0
Andrey Skorikov <proskorik@gmail.com>
2012-05-12 17:46:59 +0200

    equivalence check rewritten using corresponds

--- redundancy-removal/src/de/proskor/redundancy/RedundancyRemoval.scala
+++ redundancy-removal/src/de/proskor/redundancy/RedundancyRemoval.scala
@@ -5,16 +5,9 @@ import de.proskor.cft.model._
 object RedundancyRemoval {
   def equivalent(first: Source, second: Source): Boolean = (first, second) match {
     case (Event(_), Event(_)) => first == second
-    case (And(_, left), And(_, right)) => equivalent(left, right)
-    case (Or(_, left), Or(_, right)) => equivalent(left, right)
-    case (Port(_, left), Port(_, right)) => equivalent(left, right)
+    case (And(_, left), And(_, right)) => left.toSeq.corresponds(right.toSeq)(equivalent)
+    case (Or(_, left), Or(_, right)) => left.toSeq.corresponds(right.toSeq)(equivalent)
+    case (Port(_, left), Port(_, right)) => left.toSeq.corresponds(right.toSeq)(equivalent)
     case _ => false
-  }
-
-  def equivalent(first: Set[Source], second: Set[Source]): Boolean = equivalent(first.toList, second.toList)
-
-  def equivalent(first: List[Source], second: List[Source]): Boolean = first match {
-    case Nil => second.isEmpty
-    case x :: xs => second.find(equivalent(x, _)).exists(m => equivalent(xs, second.filterNot(_ == m)))
   }
 }
