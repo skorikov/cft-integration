a7dcc17f44f7d88afe01a56610cd235df586806b
Andrey Skorikov <proskorik@gmail.com>
2012-06-19 12:13:59 +0200

    menu adjusted

--- ea-extension/src/de/proskor/CftExtension.scala
+++ ea-extension/src/de/proskor/CftExtension.scala
@@ -1,5 +1,7 @@
 package de.proskor
 
+import java.util.{List => JavaList}
+import collection.JavaConversions._
 import de.proskor.extension.ExtensionAdapter
 import de.proskor.extension.MenuItemAdapter
 import de.proskor.automation.Repository
@@ -7,25 +9,34 @@ import de.proskor.fel.impl.EventRepositoryImpl
 import de.proskor.fel.EventRepository
 import de.proskor.fel.ui.FailureEventListDialog
 import de.proskor.fel.ui.FailureEventListImpl
-
+import de.proskor.extension.MenuItem
+import de.proskor.automation.impl.RepositoryImpl
 
 class CftExtension extends ExtensionAdapter {
   override protected def createMenu = {
-    val mainMenu = new MenuItemAdapter("Main")
-    val fooMenu = new MenuItemAdapter(mainMenu, "Foo")
-    new MenuItemAdapter(fooMenu, "Action") {
-      override def invoke() {
-        val repository = Repository.instance
-        repository.write(repository.context.toString)
-      }
-    }
-    new MenuItemAdapter(mainMenu, "Failure Event List...") {
-      override def invoke() {
+    val cftMenu = new MenuItemAdapter("CFT")
+    
+    new MenuItemAdapter(cftMenu, "Failure Event List...") {
+      override def invoke {
         val er: EventRepository = new EventRepositoryImpl(Repository.instance)
         val dialog: FailureEventListDialog = new FailureEventListImpl(er)
         dialog.showEventList
       }
     }
-    mainMenu
+
+    new MenuItemAdapter(cftMenu, "Create Event Instance") {
+      override def hasChildren: Boolean = this.getChildren.size > 0
+
+      override def getChildren: JavaList[MenuItem] = {
+        val er: EventRepository = new EventRepositoryImpl(Repository.instance)
+        val items = for {
+          container <- er.getEventTypeContainers
+          event <- container.getEvents
+        } yield new MenuItemAdapter(event.getName)
+        items
+      }
+    }
+
+    cftMenu
   }
 }
