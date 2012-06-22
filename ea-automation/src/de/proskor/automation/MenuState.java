513050043025b2883ed781b3bef2b31556c37bfa
Andrey Skorikov <proskorik@gmail.com>
2012-06-22 16:46:54 +0200

    javadoc added

--- ea-automation/src/de/proskor/automation/MenuState.java
+++ ea-automation/src/de/proskor/automation/MenuState.java
@@ -1,18 +1,35 @@
 package de.proskor.automation;
 
+/**
+ * Encapsulates the state of a menu item. 
+ * This class is immutable and thread-safe.
+ */
 public final class MenuState {
 	private final boolean enabled;
 	private final boolean checked;
 
+	/**
+	 * Creates a new instance.
+	 * @param enabled true if menu item is enabled.
+	 * @param checked true if menu item is checked.
+	 */
 	public MenuState(boolean enabled, boolean checked) {
 		this.enabled = enabled;
 		this.checked = checked;
 	}
 
+	/**
+	 * Get enabled state.
+	 * @return true if menu item is enabled, false otherwise.
+	 */
 	public boolean isEnabled() {
 		return this.enabled;
 	}
 
+	/**
+	 * Get checked state.
+	 * @return true if menu item is checked, false otherwise.
+	 */
 	public boolean isChecked() {
 		return this.checked;
 	}
