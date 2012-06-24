88b546f7c4932637bb762ded7046489c73ed2b8c
Andrey Skorikov <proskorik@gmail.com>
2012-06-24 11:37:52 +0200

    refactoring

--- ea-extension/src/de/proskor/CftExtension.scala
+++ ea-extension/src/de/proskor/CftExtension.scala
@@ -1,19 +1,21 @@
 package de.proskor
 
 import java.util.{List => JavaList}
-import collection.JavaConversions._
-import de.proskor.extension.ExtensionAdapter
-import de.proskor.extension.MenuItemAdapter
+
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.JavaConversions.seqAsJavaList
+
 import de.proskor.automation.Repository
+import de.proskor.cft.test.AdapterTests
+import de.proskor.cft.test.CftTests
+import de.proskor.cft.test.PeerTests
+import de.proskor.extension.ExtensionAdapter
+import de.proskor.extension.MenuItem
+import de.proskor.extension.MenuItemAdapter
 import de.proskor.fel.impl.EventRepositoryImpl
-import de.proskor.fel.EventRepository
 import de.proskor.fel.ui.FailureEventListDialog
 import de.proskor.fel.ui.FailureEventListImpl
-import de.proskor.extension.MenuItem
-import de.proskor.automation.impl.RepositoryImpl
-import de.proskor.cft.test.AdapterTests
-import de.proskor.cft.test.PeerTests
-import de.proskor.cft.test.CftTests
+import de.proskor.fel.EventRepository
 import de.proskor.shell.EpsilonShell
 
 class CftExtension extends ExtensionAdapter {
@@ -50,18 +52,16 @@ class CftExtension extends ExtensionAdapter {
     new MenuItemAdapter(cftMenu, "Create Event Instance") {
       override def isVisible: Boolean = hasChildren
 
-      override def getChildren: JavaList[MenuItem] = {
-        val er: EventRepository = new EventRepositoryImpl(Repository.instance)
-        val items = for {
-          container <- er.getEventTypeContainers
-          event <- container.getEvents
-        } yield new MenuItemAdapter(event.getName) {
-          override def invoke() {
-            Repository.instance.write("create event instance for '" + event.getName + "'")
-          }
+      override def getChildren: JavaList[MenuItem] = for {
+        container <- eventRepository.getEventTypeContainers
+        event <- container.getEvents
+      } yield new MenuItemAdapter(event.getName) {
+        override def invoke() {
+          Repository.instance.write("create event instance for '" + event.getName + "'")
         }
-        items
       }
+
+      lazy val eventRepository: EventRepository = new EventRepositoryImpl(Repository.instance)
     }
 
     cftMenu
